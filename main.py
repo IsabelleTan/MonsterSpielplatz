5bdd946eedb49bca1a4acc7ca4daa3bd9e377596
IsabelleTan <isabelle.w.tan@gmail.com>
2018-09-18 20:38:32 +0200

    Moved step function definition to abstract class.

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ import numpy as np
 
 from monster import *
 
-erdbeerli1 = Erdbeerli(0, 0, 20, 20)
+grid_w = 20
+grid_h = 20
+
+erdbeerli1 = Erdbeerli(0, 0, grid_w, grid_h)
+pirati1 = Pirati(0, 0, grid_w, grid_h)
+brummi1 = Brummi(0, 0, grid_w, grid_h)
+fressi1 = Fressi(0, 0, grid_w, grid_h)
+glubschi1 = Glubschi(0, 0, grid_w, grid_h)
+keksi1 = Keksi(0, 0, grid_w, grid_h)
+
+
+for _ in range(3):
+    erdbeerli1.step()
+    pirati1.step()
+    brummi1.step()
+    fressi1.step()
+    glubschi1.step()
+    keksi1.step()
 
 print(erdbeerli1)
-for _ in range(7):
-    erdbeerli1.step()
-    print(erdbeerli1)
+print(pirati1)
+print(brummi1)
+print(fressi1)
+print(glubschi1)
+print(keksi1)
--- monster.py
+++ monster.py
@@ -26,3 +26,30 @@ class Erdbeerli(Monster):
         self.jump_pattern = cycle(((1, 2), (1, -2), (1, 0)))
 
 
+class Pirati(Monster):
+    def __init__(self, x, y, grid_w, grid_h):
+        super().__init__(x, y, grid_w, grid_h)
+        self.jump_pattern = cycle(((1, 2), (1, -1), (1, 2)))
+
+
+class Brummi(Monster):
+    def __init__(self, x, y, grid_w, grid_h):
+        super().__init__(x, y, grid_w, grid_h)
+        self.jump_pattern = cycle(((-1, -2), (3, 1), (-1, -1)))
+
+
+class Fressi(Monster):
+    def __init__(self, x, y, grid_w, grid_h):
+        super().__init__(x, y, grid_w, grid_h)
+        self.jump_pattern = cycle(((-1, 1), (-1, -2), (-1, 3)))
+
+
+class Glubschi(Monster):
+    def __init__(self, x, y, grid_w, grid_h):
+        super().__init__(x, y, grid_w, grid_h)
+        self.jump_pattern = cycle(((-1, 1), (0, -3), (-1, 1)))
+
+class Keksi(Monster):
+    def __init__(self, x, y, grid_w, grid_h):
+        super().__init__(x, y, grid_w, grid_h)
+        self.jump_pattern = cycle(((-1, 1), (-1, -2), (-1, 1)))
