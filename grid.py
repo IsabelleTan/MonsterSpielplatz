dbda1540d7c4b3ea17f831e352bba2bdd65078ae
Roald87 <roaldruiter@gmail.com>
2018-10-07 00:38:01 +0200

    Forgot to add changes to grid.py

--- grid.py
+++ grid.py
@@ -1,19 +1,32 @@
+import matplotlib.pyplot as plt
+
 class Grid(object):
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.grid = [
-            ['.' for _ in range(self.height)]
-            for _ in range(self.width)]
         self.monsters = []
 
     def add_monster(self, monster):
         self.monsters.append(monster)
 
+    def do_step(self):
+        for monster in self.monsters:
+            monster.step()
+
+    def do_jump(self):
+        for monster in self.monsters:
+            monster.jump()
+
+    def plot(self):
+        return None
+
     def __str__(self):
+        grid = [
+            ['.' for _ in range(self.height)]
+            for _ in range(self.width)]
         for monster in self.monsters:
             x, y = monster.get_location()
-            self.grid[20 - y][x] = monster.__class__.__name__[0]
+            grid[self.height - 1 - y][x] = monster.__class__.__name__[0]
         return '\n'.join([' '.join(['{}'.format(item) \
                                    for item in row]) \
-                                   for row in self.grid])
+                                   for row in grid])
